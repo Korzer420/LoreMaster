6f438c6786e35924f130ff6f3dcf0836730fa1a2
Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>
2022-08-30 18:39:39 -0400

    Update FlowerRingPower.cs

--- LoreMaster/LorePowers/QueensGarden/FlowerRingPower.cs
+++ LoreMaster/LorePowers/QueensGarden/FlowerRingPower.cs
@@ -16,35 +16,29 @@ public class FlowerRingPower : Power
 
     #region Event Handler
 
-    private void PlayMakerFSM_OnEnable(On.PlayMakerFSM.orig_OnEnable orig, PlayMakerFSM self)
+    private void OnConvertFloatToIntAction(On.HutongGames.PlayMaker.Actions.ConvertFloatToInt.orig_OnEnter orig, ConvertFloatToInt self)
     {
-        if (string.Equals(self.FsmName,"nailart_damage"))
+        if (string.Equals(self.Fsm.FsmComponent.FsmName, "nailart_damage") && string.Equals(self.Fsm.FsmComponent.ActiveStateName, "Set"))
         {
-            self.GetState("Set").ReplaceAction(new Lambda(() =>
+            if (Active)
             {
-                if (Active)
-                {
-                    float damageMultiplier = 1f;
+                float damageMultiplier = 1f;
 
-                    if (PlayerData.instance.GetBool(nameof(PlayerData.instance.elderbugGaveFlower)))
-                        damageMultiplier += .1f;
-                    if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenEmilitiaFlower)))
-                        damageMultiplier += .1f;
-                    if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenGodseekerFlower)))
-                        damageMultiplier += .1f;
-                    if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenOroFlower)))
-                        damageMultiplier += .1f;
-                    if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenWhiteLadyFlower)))
-                        damageMultiplier += .1f;
+                if (PlayerData.instance.GetBool(nameof(PlayerData.instance.elderbugGaveFlower)))
+                    damageMultiplier += .1f;
+                if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenEmilitiaFlower)))
+                    damageMultiplier += .1f;
+                if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenGodseekerFlower)))
+                    damageMultiplier += .1f;
+                if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenOroFlower)))
+                    damageMultiplier += .1f;
+                if (PlayerData.instance.GetBool(nameof(PlayerData.instance.givenWhiteLadyFlower)))
+                    damageMultiplier += .1f;
 
-                    // This applies onto the already modified damage value. This means that if the multiplier is 1.5f, the end damage is 3.75 times the nail damage.
-                    // With fury about 7 times (if I'm not that bad in math).
-                    self.FsmVariables.FindFsmFloat("Damage Float").Value *= damageMultiplier;
-                }
-
-                self.FsmVariables.FindFsmInt("nailDamage").Value = Convert.ToInt32(Math.Round(self.FsmVariables.FindFsmFloat("Damage Float").Value, 2));
-            })
-            { Name = "Flower Ring" }, 0);
+            // This applies onto the already modified damage value. This means that if the multiplier is 1.5f, the end damage is 3.75 times the nail damage.
+            // With fury about 7 times (if I'm not that bad in math).
+            self.floatVariable.Value *= damageMultiplier;
+            }
         }
         orig(self);
     }
@@ -55,10 +49,10 @@ public class FlowerRingPower : Power
 
     /// <inheritdoc/>
     /// <inheritdoc/>
-    protected override void Enable() => On.PlayMakerFSM.OnEnable += PlayMakerFSM_OnEnable;
+    protected override void Enable() => On.HutongGames.PlayMaker.Actions.ConvertFloatToInt.OnEnter += OnConvertFloatToIntAction;
 
     /// <inheritdoc/>
-    protected override void Disable() => On.PlayMakerFSM.OnEnable -= PlayMakerFSM_OnEnable;
+    protected override void Disable() => On.HutongGames.PlayMaker.Actions.ConvertFloatToInt.OnEnter -= OnConvertFloatToIntAction;
 
     #endregion
 }
