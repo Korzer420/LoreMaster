d6b681df55987d13daaa985b4eb7acc5a2741f6a
Exempt-Medic <60412657+Exempt-Medic@users.noreply.github.com>
2022-09-02 15:20:53 -0400

    Update MaskCharge.cs
    Simplified formula used in MaskCharge

--- LoreMaster/UnityComponents/MaskCharge.cs
+++ LoreMaster/UnityComponents/MaskCharge.cs
@@ -39,10 +39,7 @@ public class MaskCharge : MonoBehaviour
             fsm.FsmName = "set_ring_damage";
             fsm.GetState("Set").ReplaceAction(new Lambda(() =>
             {
-                int nailDamage = PlayerData.instance.GetInt(nameof(PlayerData.instance.nailDamage)) / 2;
-                if (nailDamage > 20)
-                    nailDamage = 20;
-                fsm.FsmVariables.FindFsmInt("Damage").Value = nailDamage;
+                fsm.FsmVariables.FindFsmInt("Damage").Value = Mathf.Min(20, PlayerData.instance.GetInt(nameof(PlayerData.instance.nailDamage)) / 2);
             }), 0);
         }
 
